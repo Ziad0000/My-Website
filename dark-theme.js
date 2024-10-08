d814fb1fa0f430f3ae73560e980006a25fdd7e77
Ziad0000 <120849603+Ziad0000@users.noreply.github.com>
2024-10-08 21:38:53 +0300

    Update dark-theme.js

--- dark-theme.js
+++ dark-theme.js
@@ -128,5 +128,19 @@ window.onload = () => {
 };
 
     
+let span = document.querySelector(".up");
 
+window.onscroll = function () {
+
+    this.scrollY >= 700 ?  span.classList.add("show") : span.classList.remove("show");
+};
+
+
+
+span.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
 
